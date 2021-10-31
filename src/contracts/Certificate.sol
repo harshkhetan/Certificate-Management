36dab95eba516bcb8b78cd76f1022fd1cb5ee3ce
HarshKhetan <harsh.khetan@gmail.com>
2021-10-31 14:19:54 -0700

    commit

--- src/contracts/Certificate.sol
+++ src/contracts/Certificate.sol
@@ -11,55 +11,41 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Certificate is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
 
-    uint public certificate_counter;
     string[] public certificates;
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    //certification public newCert;
-    //address owner; 
-
     // A custom data structure used to define elements of BCG Certificate
     struct certification {
         
-         uint employeeId;
-        string employeeName; 
+        address recipient;
+        string recipientName; 
         // The Program
         string program;
+        address issuer;
         // Certificate Issuance Date
         uint timeOfIssue;
-        address issuer;
-        //address recipient;
     } 
-
-
-        struct employee {
-         uint employeeId;
-        string program;
-    } 
-
-    employee[] public employeeCertificate;
 
     mapping(string => bool)  _certificateExists;
     mapping(address => string)  Issuer;
     mapping(address => string[])  recipient;
+    //mapping(string => address[]) allRecipientOfCertificate;
     mapping(uint => certification)  certificateIdentifier;
-    //mapping(address => uint[])  recipientCertificates;
-    mapping(uint => uint[])  recipientCertificates;
-
-    //uint[] recipientCertificates1;
+    mapping(address => uint[])  recipientCertificates;
     mapping(address => uint[]) public issuerCertificates;
 
     // Events
 
-    //event CertificateIssued(uint indexed employeeId, string indexed employeeName, string indexed program, address indexed issuer, address indexed recipient);
+    //event CertificateIssued(address indexed recipientAddress, string indexed recipientName, string indexed program, address indexed issuer, address indexed recipient);
     
     // Event which will be raised anytime the current certificate information is updated.
-    event certificationEvent(uint certificationEvent_employeeId, string certificationEvent_employeeName, string certificationEvent_program, address certificationEvent_issuer, string  certificationEvent_tokenURI);
+    event certificationEvent(address certificationEvent_recipientAddress, string certificationEvent_recipientName, string certificationEvent_program, address certificationEvent_issuer, string  certificationEvent_tokenURI);
     // Event which will be raised anytime the current Certificate information is updated.
     event errorEvent(string errorEvent_Description);
 
- constructor() ERC721("Certificate", "BGC") {
+//Blockchain Certificate Management
+ constructor() ERC721("Certificate", "BCM") {
         //owner = msg.sender;
     }
 
@@ -96,87 +82,56 @@ contract Certificate is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         return super.supportsInterface(interfaceId);
     }
 
-
-// // TEST FUNCTIONS
-//     // Returns the current certificate information
-//     function getCertificate() public view returns (uint, string memory, string memory, address) {
-//         return (newCert.employeeId,newCert.employeeName,newCert.program,newCert.recipient);
-//     } // getCertificate
-    
-//     // Set the certificate information
-//     function setCertificate(uint _employeeId, string memory _employeeName, string memory _program, address _recipient) public {
-//         newCert.employeeId = _employeeId;
-//         newCert.employeeName = _employeeName;
-//         newCert.program = _program;
-//         newCert.timeOfIssue = block.timestamp;
-//         newCert.issuer = owner();
-//         newCert.recipient = _recipient;
-//         emit certificationEvent(newCert.employeeId,newCert.employeeName,newCert.program);
-//     } // setCertificate
-
-
-
-function issueCertificate(uint _employeeId, string memory _employeeName, string memory _program, address _issuer,  string memory _tokenURI) public onlyOwner {
+function issueCertificate(address _recipient, string memory _recipientName, string memory _program, address _issuer,  string memory _tokenURI) public onlyOwner {
    
     require(owner() == _issuer, "Issuer do not have permission to issue a certificate");
-    require(isExist(_employeeId,_program) == false, "Requested certificate is already issued to the employee.");
+    require(isExist(_recipient,_program) == false, "Requested certificate is already issued to the recipient.");
    
+
     uint id = mintNFT(_issuer, _tokenURI);
     
     // The current certification information
     certification memory cert;
-
-        cert.employeeId = _employeeId;
-        cert.employeeName = _employeeName;
+        cert.recipient = _recipient;
+        cert.recipientName = _recipientName;
         cert.program = _program;
-        cert.timeOfIssue = block.timestamp;
         cert.issuer = owner();
-        //cert.recipient = _recipient;
+        cert.timeOfIssue = block.timestamp;
 
         certificateIdentifier[id] = cert;
-        //recipientCertificates[_recipient].push(id);
-        recipientCertificates[_employeeId].push(id);
+        recipientCertificates[_recipient].push(id);
         issuerCertificates[owner()].push(id);
-
-        employee memory newEmployee = employee(_employeeId,_program);
-        employeeCertificate.push(newEmployee);
         
-      emit certificationEvent(cert.employeeId,cert.employeeName,cert.program, cert.issuer, _tokenURI);
+      emit certificationEvent(cert.recipient,cert.recipientName,cert.program, cert.issuer, _tokenURI);
     }
 
-      function isExist (uint _employeeId, string memory _program) private view returns (bool){
-      if(employeeCertificate.length == 0)
+      function isExist (address _recipient, string memory _program) private view returns (bool){
+      if(recipientCertificates[_recipient].length == 0)
       {
           return false;
       }    
-      for (uint i = 0; i< employeeCertificate.length; i++){
-          if (employeeCertificate[i].employeeId ==_employeeId && keccak256(abi.encodePacked(employeeCertificate[i].program)) ==keccak256(abi.encodePacked(_program)))
+      for (uint i = 0; i< recipientCertificates[_recipient].length; i++){
+          uint _TokenId = recipientCertificates[_recipient][i];
+          if (keccak256(abi.encodePacked(certificateIdentifier[_TokenId].program)) == keccak256(abi.encodePacked(_program)))
                return true;
          //break;
-         
+        
       }
       return false;
 
   }
 
-// function retrieveCertificate(address _recipient) public view returns (uint, string memory, string memory,uint, address, address, string memory){
-//     uint _latest = recipientCertificates[_recipient].length;
-//     uint _lastTokenId = recipientCertificates[_recipient][_latest-1];
-//     certification memory cert = certificateIdentifier[_lastTokenId];
-//     return (cert.employeeId,cert.employeeName,cert.program, cert.timeOfIssue, cert.issuer, cert.recipient, tokenURI(_lastTokenId));
-
-//     }
-
-    function retrieveCertificate(uint _employeeId) public view returns (uint, string memory, string memory,uint, address, string memory){
-    require(_employeeId > 0,'Require a valid Employee Id');
-    require(recipientCertificates[_employeeId].length > 0,'No certification to retrieve for the Employee Id');
-    uint _latest = recipientCertificates[_employeeId].length;
-    uint _lastTokenId = recipientCertificates[_employeeId][_latest-1];
+function retrieveCertificate(address _recipient) public view returns (address, string memory, string memory, address, string memory, uint){
+    require(_recipient == address(_recipient),"Invalid address");
+    require(recipientCertificates[_recipient].length > 0,'No certification to retrieve for the recipient');
+    uint _latest = recipientCertificates[_recipient].length;
+    uint _lastTokenId = recipientCertificates[_recipient][_latest-1];
     certification memory cert = certificateIdentifier[_lastTokenId];
-    return (cert.employeeId,cert.employeeName,cert.program, cert.timeOfIssue, cert.issuer, tokenURI(_lastTokenId));
+    return (cert.recipient,cert.recipientName,cert.program, cert.issuer,tokenURI(_lastTokenId),cert.timeOfIssue);
 
     }
-    
+
+      
     // function getIssuer(address _issuer) public view returns (string memory) {
     //     return Issuer[_issuer];
     // }
